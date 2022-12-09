3abebab1d66d26f789d32dfac674c4500ac974ff
Xiuwen Zhu <zhuxiuwen6@gmail.com>
2022-12-09 17:49:32 +0000

    affirmation folder

--- scripts.js
+++ scripts.js
@@ -210,3 +210,12 @@ window.addEventListener('DOMContentLoaded', event => {
 
   });
 })();
+
+
+$(document).ready(() => {
+  $(document.body).on('click', '.card[data-clickable=true]', (e) => {
+    var href = $(e.currentTarget).data('href');
+    window.location = href;
+  });
+});
+
