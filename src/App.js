a44456c70ebf8a0c3e2a4c2a3cfe7899bb6336bd
Xiuwen Zhu <zhuxiuwen6@gmail.com>
2022-12-10 01:50:27 +0000

    move to VS Code

--- src/App.js
+++ src/App.js
@@ -1,73 +1,46 @@
-import React, { useState } from 'react'
-import { Layout, Button, List } from 'antd'
-import { DownloadOutlined } from '@ant-design/icons'
+import React from "react";
+import axios from 'axios';
 
-const config = {
-  apiUrl: 'https://type.fit/api/quotes',
-  repoUrl: 'https://github.com/ssokurenko/quotes-react-app'
+import './App.css';
+
+class App extends React.Component {
+    state = {
+        advice: ""
+    }
+// fetchingQuote 
+    componentDidMount() {
+        this.fetchQuote();
+       
+    }
+//func to fetch quote 
+    fetchQuote = () => {
+        axios.get('https://api.adviceslip.com/advice')
+            .then((response) => {
+                const { advice } = response.data.slip;
+                
+                this.setState({advice});
+
+
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+
+    }
+
+
+    render(){
+        const { advice } = this.state;
+        return (
+            <div className="app">
+                <div className="card">
+                <h1 className="heading">{advice}</h1>
+                </div>
+
+            </div>
+            
+        );
+    }
 }
 
-const { Header, Content } = Layout
-
-function App() {
-  const [quotes, setQuotes] = useState([])
-  const [isLoading, setIsLoading] = useState(false)
-  const Quote = ({ text, author }) => {
-    return (
-      <span>
-        <strong>{text}</strong> &nbsp; <span>{author}</span>
-      </span>
-    )
-  }
-
-  const getQuotes = () => {
-    setQuotes([])
-    setIsLoading(true)
-    fetch(config.apiUrl)
-      .then(function (response) {
-        return response.json()
-      })
-      .then((data) => {
-        setQuotes(data)
-        setIsLoading(false)
-      })
-      .catch(() => {
-        setIsLoading(false)
-      })
-  }
-  return (
-    <Layout>
-      <Header>
-        <div className="container">
-          <span className="site-logo">Inspirational Quotes</span>
-        </div>
-      </Header>
-      <Content className="container">
-        <List
-          size="small"
-          loading={isLoading}
-          header={
-            <Button
-              onClick={() => getQuotes()}
-              type="primary"
-              icon={<DownloadOutlined />}
-              disabled={isLoading}
-              size="large">
-              Fetch Quotes
-            </Button>
-          }
-          footer={<a href={config.repoUrl}>Fork on Github</a>}
-          bordered
-          dataSource={quotes}
-          renderItem={(quote) => (
-            <List.Item>
-              <Quote text={quote.text} author={quote.author} />
-            </List.Item>
-          )}
-        />
-      </Content>
-    </Layout>
-  )
-}
-
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -18,22 +18,69 @@
 // }
 // root.render(<MyApp />);
 
-'use strict';
+// import React from 'react'
+// import { createRoot } from 'react-dom/client';
+// import App from '/src/App.js'
 
-const e = React.createElement;
 
-class LikeButton extends React.Component {
-  render() {
+const element = document.getElementById('root');
+const root = createRoot(element!);
 
-      return 'hello world';
-    
+class App extends React.Component {
+    state = {
+        advice: ""
+    }
+// fetchingQuote 
+    componentDidMount() {
+        this.fetchQuote();
+       
+    }
+//func to fetch quote 
+    fetchQuote = () => {
+        axios.get('https://api.adviceslip.com/advice')
+            .then((response) => {
+                const { advice } = response.data.slip;
+                
+                this.setState({advice});
 
-  }
+
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+
+    }
+
+
+    render(){
+        const { advice } = this.state;
+        return (
+            <div className="app">
+                <div className="card">
+                <h1 className="heading">{advice}</h1>
+                </div>
+
+            </div>
+            
+        );
+    }
 }
 
+// IT WORKS!!
+// const e = React.createElement;
 
-const domContainer = document.querySelector('#like_button_container');
-const root = ReactDOM.createRoot(domContainer);
-root.render(e(LikeButton));
+// class LikeButton extends React.Component {
+//   render() {
+
+//       return 'hello world';
+    
+
+//   }
+// }
+
+
+// const domContainer = document.querySelector('#like_button_container');
+// const root = ReactDOM.createRoot(domContainer);
+// root.render(e(LikeButton));
 
 
